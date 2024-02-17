c6465c7b6528de6ce09c73b4d1cd1d2beab6b678
Anton Yermolayev <Anton_Yermolayev@epam.com>
2024-02-17 16:24:21 +0600

    WIP Add MVC project, Add Repository project

---
+++ Drivers/Controllers/DriversController.cs
@@ -0,0 +1,35 @@
+﻿using Drivers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Drivers.Controllers
+{
+	public class DriversController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public DriversController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Add()
+		{
+			return Content("Adding a new driver");
+		}
+
+
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
